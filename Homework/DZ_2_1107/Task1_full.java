bb3caa4c9330b660d700ff67c845fd6f56e264f6
PavelKruglikov <pavelkruglikov@yahoo.com>
2022-07-18 21:27:39 +0300

    fixed

--- Homework/DZ_2_1107/Task1_full.java
+++ Homework/DZ_2_1107/Task1_full.java
@@ -7,14 +7,14 @@ public class Task1_full {
         System.out.print("Введите размер ");
         Scanner sc1 = new Scanner(System.in);
         int size = sc1.nextInt();
-        for (int i = 0; i <= size-1; i++) {
+        for (int i = 0; i < size; i++) {
             for (int j = size; j > i; j--) {
                 System.out.print(" ");
             }
             for (int j1 = 0; j1 <= i; j1++) {
                 System.out.print("*");
             }
-            System.out.println(" ");
+            System.out.println();
         }
     }
 }
