ae656c6f697254dc2305199c9a3243091d776efb
PavelKruglikov <pavelkruglikov@yahoo.com>
2022-07-21 12:31:00 +0300

    Homework3 is done

--- Homework/DZ_3_1807/Task6_Storage.java
+++ Homework/DZ_3_1807/Task6_Storage.java
@@ -1,3 +1,10 @@
 package DZ_3_1807;
+public class Task6_Storage {
+    public int storage (String s){
+        return s.length() * 2;
+    }
 
-public class Task6_Storage {}
+    public static void main(String[] args) {
+        Task6_Storage call = new Task6_Storage();
+    }
+}
--- Homework/DZ_3_1807/Task8_Velocity.java
+++ Homework/DZ_3_1807/Task8_Velocity.java
@@ -4,27 +4,23 @@ import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Task8_Velocity {
-    final float CONSTDISTANCE = getDistance();
-    final float CONSTTIME = getTime();
-    final float CONSTVELOCITY = CONSTDISTANCE/CONSTTIME;
 
-    private float getDistance() {
+    private static final float GETDISTANCE(){
         System.out.print("Введите расстояние в метрах ");
         Scanner sc1 = new Scanner(System.in);
         float getDistance = sc1.nextFloat();
         return getDistance;
     }
-    private final float getTime() {
+    private static final float GETTIME(){
         System.out.print("Введите расстояние в минутах ");
         Scanner sc2 = new Scanner(System.in);
         float getTime = sc2.nextFloat();
         return getTime;
     }
     public static void main(String[] args) {
-            Task8_Velocity vel = new Task8_Velocity();
-            DecimalFormat df = new DecimalFormat("##.##");
-            System.out.println("При расстоянии " + vel.CONSTDISTANCE + " и времени " + vel.CONSTTIME);
-            System.out.print("cкорость равняется "+df.format(vel.CONSTVELOCITY)+" м/с");
+        float velocity = (GETDISTANCE()/GETTIME());
+        DecimalFormat df = new DecimalFormat("##.##");
+        System.out.print("cкорость равняется "+df.format(velocity)+" м/с");
     }
 }
 
