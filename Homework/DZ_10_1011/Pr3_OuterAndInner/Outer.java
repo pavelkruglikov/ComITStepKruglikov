156bc0db47a49d94b3dd02f2f4daad4bcc68bb8b
PavelKruglikov <pavelkruglikov@yahoo.com>
2022-11-14 17:39:18 +0300

    Homework in progress 3

--- Homework/DZ_10_1011/Pr3_OuterAndInner/Outer.java
+++ Homework/DZ_10_1011/Pr3_OuterAndInner/Outer.java
@@ -1,12 +1,27 @@
 package DZ_10_1011.Pr3_OuterAndInner;
 
+/*1) Напишите класс с именем Outer, содержащий
+        внутренний класс с именем Inner. Добавьте метод во внешний
+        который возвращает объект типа Inner. В
+        main(), создать и инициализировать ссылку на
+        внутренний. */
+
 public class Outer {
 
-    public static void getInner(){
+    private String stringFiled;
+
+    public Outer(String stringFiled) {
+        this.stringFiled = stringFiled;
+    }
+
+    public static void getInner() {
         Inner createObject = new Inner();
     }
+
     static class Inner {
-        public void sayHello(){
+
+
+        public void sayHello() {
             System.out.println("Hello from inner class");
         }
 
